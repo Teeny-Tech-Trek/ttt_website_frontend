02a036c82f1471440b22e3f022f289e04bf5e11f
pawanxandromeda <pawanxandromeda@gmail.com>
2025-06-17 02:01:53 -0700

    Update axios.ts

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://ttt-backend-nb12.onrender.com/api',
+  baseURL: 'https://ttt-backend-fzbp.onrender.com',
   withCredentials: true, // so cookies (refresh token) are sent
   headers: { 'Content-Type': 'application/json' },
 });
